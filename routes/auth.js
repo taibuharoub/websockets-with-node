8433f7d0ce7fbc9fdeb14d4c3041cd79b78ab72f
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-19 18:11:22 +0300

    Add auth controllers

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 const User = require("../models/User");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -28,5 +29,19 @@ router.put(
 );
 
 router.post("/login", authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch(
+  '/status',
+  isAuth,
+  [
+    body('status')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.updateUserStatus
+);
 
 module.exports = router;
